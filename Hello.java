d5935e94799ecc6eb71ed7c954c18fce576b2372
Dmytro <cheroky.art@gmail.com>
2026-07-12 18:27:32 +0200

    add more numbers

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		int number1 = 15;
-		int number2 = 25;
+		int number2 = 35;
 		System.out.println("Сумма :" + (number1 + number2));
 	}
 }
