64ccb52b1cfacf199e52bfb0e16337571c5a4534
Dmytro <cheroky.art@gmail.com>
2026-07-12 17:53:06 +0200

    ADD new operation

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		int number1 = 15;
+		int number2 = 25;
+		System.out.println("Сумма :" + (number1 + number2));
 	}
 }
